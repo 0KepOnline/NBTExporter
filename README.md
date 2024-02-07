0b55832eb72b70c7b1e309a6a118c8cde86c8b14
0KepOnline <78259495+0KepOnline@users.noreply.github.com>
2024-02-07 16:47:49 +0200

    v1.1: Release for 1.19 & 1.20, minor code changes

---
+++ README.md
@@ -0,0 +1,20 @@
+# ![Logo](src/main/resources/nbtexporter_logo_32x32.png) NBT Exporter
+
+NBT Exporter is a simple client-side mod that allows players to export NBT data of items from their inventory.
+
+## Usage
+![!](https://img.shields.io/static/v1?label=&message=!&color=yellow) This mod only exports NBT data, **not item IDs, stack counts, or damage values**.
+
+Hold the item you want to export NBT for, and use the `/exportnbt` command.
+
+You can use it like that:
+```
+/exportnbt MyUniqueItem
+```
+
+...or like that:
+```
+/exportnbt "My Unique Item, but with spaces"
+```
+
+Providing a file name is optional. If not specified, the file will use the current date and time as the name.
---
+++ src/main/java/net/scenariopla/nbtexporter/NBTExporter.java
@@ -0,0 +1,57 @@
+package net.scenariopla.nbtexporter;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.regex.Pattern;
+
+import net.minecraft.client.Minecraft;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.RegisterClientCommandsEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import net.scenariopla.nbtexporter.command.ExportItemStackNBTCommand;
+import net.scenariopla.nbtexporter.init.DirectoryInit;
+
+@Mod(NBTExporter.Reference.MODID)
+public class NBTExporter {
+    public class Reference {
+        public static final String MODID = "nbtexporter";
+        public static final String NAME = "NBT Exporter";
+        public static final String VERSION = "1.1";
+        
+        public static final String MODDIR = MODID;
+        
+        public static final Minecraft MINECRAFT = Minecraft.getInstance();
+        public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd_HH.mm.ss");
+    
+        public static final String FILE_NAME_CHAR_REPLACE = "<>:\"/\\|?*\r\n\t\b\f";
+        public static final Pattern FILE_NAME_CHAR_REPLACE_PATTERN = Pattern.compile("[" + FILE_NAME_CHAR_REPLACE + "]");
+    }
+    
+    public static File[] modFiles = null;
+
+    public NBTExporter() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::init);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+    
+    public void init(FMLClientSetupEvent event) {
+        DirectoryInit.modDir = new File(Reference.MINECRAFT.gameDirectory, Reference.MODDIR);
+        modFiles = DirectoryInit.initModDirectory();
+    }
+    
+    @Mod.EventBusSubscriber
+    public class EventHandler {
+        @SubscribeEvent
+        @OnlyIn(Dist.CLIENT)
+        public static void onRegisterCommands(RegisterClientCommandsEvent event) {
+            ExportItemStackNBTCommand.register(event.getDispatcher());
+        }
+    }
+}
